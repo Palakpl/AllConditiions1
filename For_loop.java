6dfd993f170ed13f274fa186e7446adbb32a346a
Palak Panchal <pn_panchal28@yahoo.com>
2022-07-29 00:42:24 +0100

    Commit for loops

---
+++ For_loop.java
@@ -0,0 +1,21 @@
+package B.Loops;
+
+public class For_loop {
+
+    public static void main(String[] args) {
+        //int num;
+        System.out.println("Printing 5 table:-");
+        for(int i=5;i<6;i++)
+        {
+            for(int j=1;j<=10;j++)
+            {
+
+                System.out.println(i + "*" + j + " = " + i*j);
+            }
+
+
+        }
+
+
+    }
+}
